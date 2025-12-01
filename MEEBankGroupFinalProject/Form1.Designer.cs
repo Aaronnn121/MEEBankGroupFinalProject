31ef03ebe1b691a189b7eec5b5e3d12438138f0d
MarkAaronEstanoco <MarkAaronEstanoco@SMU-0032>
2025-12-01 17:22:50 +0800

    Add project files.

---
+++ MEEBankGroupFinalProject/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MEEBankGroupFinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Britannic Bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(113, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 53);
+            label1.TabIndex = 2;
+            label1.Text = "MEEBank";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(64, 0, 0);
+            pictureBox1.Location = new Point(67, 85);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(297, 342);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(182, 354);
+            button1.Name = "button1";
+            button1.Size = new Size(68, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Sign Up";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ActiveCaptionText;
+            label4.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(182, 283);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 18);
+            label4.TabIndex = 14;
+            label4.Text = "Password";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 257);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(153, 23);
+            textBox3.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(193, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 18);
+            label3.TabIndex = 12;
+            label3.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(191, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 18);
+            label2.TabIndex = 11;
+            label2.Text = "Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 199);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(153, 23);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(141, 142);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(153, 23);
+            textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(429, 450);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            ForeColor = SystemColors.ActiveCaption;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
